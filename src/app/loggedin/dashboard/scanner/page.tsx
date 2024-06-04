d38b2f4f0759f14aea761fb1badc84acb1884242
Constantin Debertin <57562411+constantindebertin@users.noreply.github.com>
2024-06-04 10:15:23 +0200

    nr2

--- src/app/loggedin/dashboard/scanner/page.tsx
+++ src/app/loggedin/dashboard/scanner/page.tsx
@@ -221,7 +221,7 @@ function ScannerComp({ user }) {
                                     //@ts-ignore
                                     setEmail(result.rawValue);
                                     console.log("Res:", result);
-                                    alert(result.rawValue);
+                                    alert(result);
                                     //@ts-ignore
                                     if(result.rawValue.endsWith("@igs-buchholz.de")){
                                         console.log("E-Mail passed:", email)
