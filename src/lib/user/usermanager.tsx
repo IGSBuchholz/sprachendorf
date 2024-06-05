6f7191c9e5e717659e72b6e1e63e21f76f5c7daa
Constantin Debertin <57562411+constantindebertin@users.noreply.github.com>
2024-06-06 00:12:18 +0200

    removed canada, also added some  minor checks

--- src/lib/user/usermanager.tsx
+++ src/lib/user/usermanager.tsx
@@ -2,10 +2,11 @@ import { eq } from 'drizzle-orm';
 import { getDatabaseConnection } from '../databsemanager'; // Assuming this is the file with the previously defined methods.
 import { User, users, NewUser } from './user';
 import {PostgresJsDatabase} from "drizzle-orm/postgres-js";
-import {Course, courses} from "@/lib/conutries";
+import {Course, courses, NewCourse} from "@/lib/conutries";
 import {getCountries} from "@/lib/countriesmanager";
 import {Simulate} from "react-dom/test-utils";
 import select = Simulate.select;
+import {integer, serial, text} from "drizzle-orm/pg-core";
 
     export async function getUser(emailAdress: string): Promise<User | undefined> {
         const db = await getDatabaseConnection();
@@ -43,7 +44,14 @@ import select = Simulate.select;
     export async function checkCountry(user: User): Promise<User>{
         const connection = await getDatabaseConnection();
 
-        if(user.startcountry == "Polen"){
+        if(user.isAdmin){
+            if(user.startcountry != "" || user.startcountry != null){
+                await connection.update(users).set({startcountry: ""}).where(eq(users.email, user.email.toLowerCase()))
+                user.startcountry = ""
+            }
+        }
+
+        if(user.startcountry == "Polen" || user.startcountry == "Kanada"){
             const chosenCountry = await chooseCountry(connection);
             await connection.update(users).set({startcountry: chosenCountry.country}).where(eq(users.email, user.email.toLowerCase()))
             user.startcountry = chosenCountry.country
@@ -52,7 +60,12 @@ import select = Simulate.select;
         return user;
     }
 
-    export async function chooseCountry(connection: PostgresJsDatabase) : Promise<Course>{
+    const nonCourse: Course = {id: 999, country: "ERROR", levels: 10, imglink: "/", maxusers: 999}
+
+    export async function chooseCountry(connection: PostgresJsDatabase, timesCalled = 0) : Promise<Course>{
+        if(timesCalled > 10){
+            return nonCourse;
+        }
         const countries = await getCountries()
         console.log("WW" + Math.floor(Math.random()*countries.length))
         var country = countries[Math.floor(Math.random()*countries.length)];
@@ -60,7 +73,7 @@ import select = Simulate.select;
         var usersAlready = await connection.select().from(users).where(eq(users.startcountry, country.country));
         const selectCntry = await connection.select().from(courses).where(eq(courses.country, country.country));
         if (selectCntry[0].maxusers && usersAlready.length >= selectCntry[0].maxusers) {
-            return chooseCountry(connection);
+            return chooseCountry(connection, timesCalled++);
         }
         return country;
     }
